1cbb310df96f3281a9b023cc6b201fdf6064a2d8
Anu Apiti <anuapt@gnail.com>
2017-11-24 08:32:48 -0800

    Feat: create dummy data for api consumption

---
+++ server/model/center.model.js
@@ -0,0 +1,72 @@
+export let centers = [
+    {
+      id: 1,
+      name: 'JavaScript Conference',
+      description: 'Developers meetup',
+      capacity: 980,
+	  center: 'Hall'
+    },
+   {
+      id: 2,
+      name: 'Andela Fellow Conference',
+      description: 'Dinner and career clinic for members',
+      capacity: 580,
+	  center: 'Marque Hall'
+    },
+    {
+      id: 3,
+      name: 'Python Conference',
+      description: 'Python Hackathon for members',
+      capacity: 200,
+	  center: 'Boardroom', 
+    },
+    {
+      id: 4,
+      name: 'Front Conference',
+      description: 'Frontend developers meetup',
+      capacity: 250,
+	  center: 'Big Hall'
+    },
+  {
+      id: 5,
+      name: 'GDG Lagos Meetup',
+      description: 'Lagos Developer Meetup',
+      capacity: 280,
+	    center: 'Banquet Hall'
+    },
+	 {
+      id: 6,
+      name: 'JavaScript Conference',
+      description: 'Developers meetup',
+      capacity: 980,
+	  center: 'Banquet Hall'
+    },
+   {
+      id: 7,
+      name: 'Andela Fellow Conference',
+      description: 'Dinner and career clinic for members',
+      capacity: 580,
+	  center: 'Big Hall'
+    },
+    {
+      id: 8,
+      name: 'Python Conference',
+      description: 'Python Hackathon for members',
+      capacity: 200,
+	  center: 'Dinner Hall'
+    },
+    {
+      id: 9,
+      name: 'Front Conference',
+      description: 'Frontend developers meetup',
+      capacity: 250,
+	  center: 'Banquet Hall'
+    },
+  {
+      id: 10,
+      name: 'GDG Lagos Meetup',
+      description: 'Peter Bacon Darwin',
+      capacity: 280,
+	  center: 'Hall'
+    }
+  ]
---
+++ server/model/event.model.js
@@ -0,0 +1,104 @@
+const events = [
+    {
+      id: 1,
+      name: 'JavaScript Conference',
+      purpose: 'Developers meetup',
+      date: '23-12-2017',
+      duration: '7 days',
+      time: '9:00 am',
+      guests: '980',
+	    center: 'Hall'
+    },
+   {
+      id: 2,
+      name: 'Andela Fellow Conference',
+      purpose: 'Dinner and career clinic for members',
+      date: '25-11-2017',
+      duration: '7 days',
+      time: '9:00 am',
+      guests: '580',
+	    center: 'Marque Hall'
+    },
+    {
+      id: 3,
+      name: 'Python Conference',
+      purpose: 'Python Hackathon for members',
+      date: '23-11-2017',
+      duration: '7 days',
+      time: '9:00 am',
+      guests: 200,
+	    center: 'Boardroom', 
+    },
+    {
+      id: 4,
+      name: 'Front Conference',
+      purpose: 'Frontend developers meetup',
+      date: '23-56-2105',
+      duration: '7 days',
+      time: '9:00 am',
+      guests: '250',
+	    center: 'Big Hall'
+    },
+  {
+      id: 5,
+      name: 'GDG Lagos Meetup',
+      purpose: 'Peter Bacon Darwin',
+      date: '4-5-2019',
+      duration: '7 days',
+      time: '9:00 am',
+      guests: '280',
+	    center: 'Banquet Hall'
+    },
+	 {
+      id: 6,
+      name: 'JavaScript Conference',
+      purpose: 'Developers meetup',
+      date: '23-12-2017',
+      duration: '7 days',
+      time: '9:00 am',
+      guests: '980',
+	  center: 'Banquet Hall'
+    },
+   {
+      id: 7,
+      name: 'Andela Fellow Conference',
+      purpose: 'Dinner and career clinic for members',
+      date: '25-11-2017',
+      duration: '7 days',
+      time: '9:00 am',
+      guests: '580',
+	  center: 'Big Hall'
+    },
+    {
+      id: 8,
+      name: 'Python Conference',
+      purpose: 'Python Hackathon for members',
+      date: '23-11-2017',
+      duration: '7 days',
+      time: '9:00 am',
+      guests: '200',
+	  center: 'Dinner Hall'
+    },
+    {
+      id: 9,
+      name: 'Front Conference',
+      purpose: 'Frontend developers meetup',
+      date: '4-5-2345',
+      duration: '7 days',
+      time: '9:00 am',
+      guests: '250',
+	  center: 'Banquet Hall'
+    },
+  {
+      id: 10,
+      name: 'GDG Lagos Meetup',
+      purpose: 'Peter Bacon Darwin',
+      date: '3-3-4344',
+      duration: '7 days',
+      time: '9:00 am',
+      guests: '280',
+	  center: 'Hall'
+    }
+  ]
+
+  export default events;
